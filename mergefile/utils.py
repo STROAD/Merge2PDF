7ba004dbb54968a1c731e5b1b3e7be3e478bd0c4
STROAD <koseok0611@naver.com>
2023-12-10 13:11:56 +0900

    병합하는 함수 추가

--- mergefile/utils.py
+++ mergefile/utils.py
@@ -1,1 +1,25 @@
 import fitz
+
+
+def merge_to_pdf(files_list, save_dir, save_name, save_file_extension):
+    """PDF로 병합
+
+    Args:
+        files_list (List[str]): 병합할 파일들의 경로가 담겨있는 리스트
+        save_dir (str): 병합된 파일을 저장할 경로
+        save_name (str): 병합된 파일을 저장할 이름
+        save_file_extension (str): 병합된 파일을 저장할 파일 확장자
+
+    """
+
+
+def merge_to_img(files_list, save_dir, save_name, save_file_extension):
+    """이미지(.png or .jpg)로 병합
+
+    Args:
+        files_list (List[str]): 병합할 파일들의 경로가 담겨있는 리스트
+        save_dir (str): 병합된 파일을 저장할 경로
+        save_name (str): 병합된 파일을 저장할 이름
+        save_file_extension (str): 병합된 파일을 저장할 파일 확장자
+
+    """
