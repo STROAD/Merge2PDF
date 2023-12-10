c2d64b6fb022fa3f863b7f662cd1dbcbc43e80ef
STROAD <koseok0611@naver.com>
2023-12-10 13:39:23 +0900

    오타 수정

--- mergefile/utils.py
+++ mergefile/utils.py
@@ -1,25 +1,25 @@
 import fitz
 
 
-def merge_to_pdf(files_list, save_dir, save_name, save_file_extension):
+def merge_to_pdf(files_list, save_dir, save_name, save_format):
     """PDF로 병합
 
     Args:
         files_list (List[str]): 병합할 파일들의 경로가 담겨있는 리스트
         save_dir (str): 병합된 파일을 저장할 경로
         save_name (str): 병합된 파일을 저장할 이름
-        save_file_extension (str): 병합된 파일을 저장할 파일 확장자
+        save_format (str): 병합된 파일을 저장할 파일 확장자
 
     """
 
 
-def merge_to_img(files_list, save_dir, save_name, save_file_extension):
+def merge_to_img(files_list, save_dir, save_name, save_format):
     """이미지(.png or .jpg)로 병합
 
     Args:
         files_list (List[str]): 병합할 파일들의 경로가 담겨있는 리스트
         save_dir (str): 병합된 파일을 저장할 경로
         save_name (str): 병합된 파일을 저장할 이름
-        save_file_extension (str): 병합된 파일을 저장할 파일 확장자
+        save_format (str): 병합된 파일을 저장할 파일 확장자
 
     """
