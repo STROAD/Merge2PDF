c1fdc512ce719159e0f03022887f6dd4f82ee322
STROAD <koseok0611@naver.com>
2023-12-26 11:25:44 +0900

    test_is_valid_path 추가

--- test/test_utils.py
+++ test/test_utils.py
@@ -6,3 +6,18 @@ from re import match
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from merge2pdf import utils
+
+
+def test_is_valid_path():
+    valid_path = os.getenv("USERPROFILE")
+    assert utils.is_valid_path(valid_path) is True
+
+    relative_path = "..\\merge2pdf"
+    assert utils.is_valid_path(relative_path) is False
+
+    non_existing_path = "/non_existing_directory"
+    assert utils.is_valid_path(non_existing_path) is False
+
+    assert utils.is_valid_path("") is False
+
+    assert utils.is_valid_path(None) is False
