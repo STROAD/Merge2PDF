124c76e3cb0d6ce1baf50a17c3044e1b7d53ce3d
STROAD <koseok0611@naver.com>
2023-12-26 11:08:44 +0900

    import

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,8 @@
+import sys
 import os
 from tempfile import gettempdir
 import fitz
 from re import match
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+from merge2pdf import utils
